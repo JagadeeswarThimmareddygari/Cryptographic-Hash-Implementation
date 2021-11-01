3944d56049d7d8cb72c19901e39802c6a4ba99f4
Arun Kumar <arunkumar.aqm@gmail.com>
2021-11-01 12:00:27 +0530

    Small update

--- sha.py
+++ sha.py
@@ -3,7 +3,7 @@
 import struct
 from constants import *
 
-_k = FRACTIONAL_PARTS_OF_CUBES_OF_PRIMES
+_k = FRACTIONAL_PARTS_OF_CUBERTS_OF_PRIMES
 
 # Mask off to 64 bit word
 def mask(a):
@@ -17,17 +17,17 @@ def rr(x,y):
     return mask(three) 
 
 # Two standard mix functions for the state expand
-def f0(w):
-    one = rr(w[-15], 1)
-    two = rr(w[-15], 8)
-    three = (w[-15] >> 7)
-    res = one ^ two ^ three
-    return res
-
 def f1(w):
     one = rr(w[-2], 19)
     two = rr(w[-2], 61)
     three = (w[-2] >> 6)
+    res = one ^ two ^ three
+    return res
+
+def f2(w):
+    one = rr(w[-15], 1)
+    two = rr(w[-15], 8)
+    three = (w[-15] >> 7)
     res = one ^ two ^ three
     return res
 
@@ -37,7 +37,7 @@ def expand_state(c):
     
     for i in range(16, 80):
         mix1 = f1(w)
-        mix2 = f0(w)
+        mix2 = f2(w)
         three = w[-16]
         four = w[-7]
         summ = mix1 + mix2 + three + four
@@ -149,8 +149,8 @@ def do_some_tests():
         {"m":"abc", "r256": "ba7816bf 8f01cfea 414140de 5dae2223 b00361a3 96177a9c b410ff61 f20015ad", "r512": "ddaf35a193617aba cc417349ae204131 12e6fa4e89a97ea2 0a9eeee64b55d39a 2192992a274fc1a8 36ba3c23a3feebbd 454d4423643ce80e 2a9ac94fa54ca49f"},
         {"m": "", "r256": "e3b0c442 98fc1c14 9afbf4c8 996fb924 27ae41e4 649b934c a495991b 7852b855", "r512": "cf83e1357eefb8bd f1542850d66d8007 d620e4050b5715dc 83f4a921d36ce9ce 47d0d13c5d85f2b0 ff8318d2877eec2f 63b931bd47417a81 a538327af927da3e"}, 
         {"m": "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", "r256": "248d6a61 d20638b8 e5c02693 0c3e6039 a33ce459 64ff2167 f6ecedd4 19db06c1", "r512": "204a8fc6dda82f0a 0ced7beb8e08a416 57c16ef468b228a8 279be331a703c335 96fd15c13b1b07f9 aa1d3bea57789ca0 31ad85c7a71dd703 54ec631238ca3445"}, 
-        # {"m": "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu", "r256": "cf5b16a7 78af8380 036ce59e 7b049237 0b249b11 e8f07a51 afac4503 7afee9d1", "r512": "8e959b75dae313da 8cf4f72814fc143f 8f7779c6eb9f7fa1 7299aeadb6889018 501d289e4900f7e4 331b99dec4b5433a c7d329eeb6dd2654 5e96e55b874be909"}, 
-        # {"m": "a"*1000000, "r256": "cdc76e5c 9914fb92 81a1c7e2 84d73e67 f1809a48 a497200e 046d39cc c7112cd0", "r512": "e718483d0ce76964 4e2e42c7bc15b463 8e1f98b13b204428 5632a803afa973eb de0ff244877ea60a 4cb0432ce577c31b eb009c5c2c49aa2e 4eadb217ad8cc09b"}
+        {"m": "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu", "r256": "cf5b16a7 78af8380 036ce59e 7b049237 0b249b11 e8f07a51 afac4503 7afee9d1", "r512": "8e959b75dae313da 8cf4f72814fc143f 8f7779c6eb9f7fa1 7299aeadb6889018 501d289e4900f7e4 331b99dec4b5433a c7d329eeb6dd2654 5e96e55b874be909"}, 
+        {"m": "a"*1000000, "r256": "cdc76e5c 9914fb92 81a1c7e2 84d73e67 f1809a48 a497200e 046d39cc c7112cd0", "r512": "e718483d0ce76964 4e2e42c7bc15b463 8e1f98b13b204428 5632a803afa973eb de0ff244877ea60a 4cb0432ce577c31b eb009c5c2c49aa2e 4eadb217ad8cc09b"}
     ]
     # for tv in tvs[:-1]:  # Remove [:-1] to not skip recursion-killer-size case
     for tv in tvs:
@@ -171,7 +171,7 @@ def do_some_tests():
 if __name__ == '__main__':
     do_some_tests()
 
-    # m = 'abc'
+    # m = ''
     # ncr = sha512(m.encode())
     # calcr = (''.join("%02x"%(a) for a in ncr))
     # print(calcr)
